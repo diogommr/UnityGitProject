aa5dab3e6c925c2757b2a36391a3bfd374ec78d5
Diogo Ribeiro <diogommr@gmail.com>
2017-09-19 15:23:53 +0100

    Project import.

---
+++ Assets/Ads.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class Ads : MonoBehaviour {
+	public void ShowAd() {
+		if (Advertisement.IsReady()) {
+			Advertisement.Show();
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		//ShowAd();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ShowAd();
+	}
+}
---
+++ Assets/AnalyticsScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Analytics;
+using System.Collections.Generic;
+
+public class AnalyticsScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		LevelUp();
+		Transaction();
+		DemographicInfo();
+	}
+
+	void LevelUp() {
+		Analytics.CustomEvent("levelUp", new Dictionary<string, object>
+		{
+			{ "newLevel", 2 },
+			{ "trigger", "purchase" }
+		});
+	}
+
+	void Transaction() {
+		Analytics.Transaction("levelUp", 1.00m, "EUR", null, null);
+	}
+
+	void DemographicInfo() {
+		Gender gender = Gender.Male;
+		Analytics.SetUserGender(gender);
+
+		int birthYear = 1994;
+		Analytics.SetUserBirthYear(birthYear);
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
